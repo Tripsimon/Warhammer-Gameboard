cc27c17aed0abc6d6a44a1b1f0eecd5dd4d26645
Martin Lešák <lesak@cis.cz>
2023-09-26 13:54:04 +0200

    Obrazkyy

--- client/src/components/Gameboard.js
+++ client/src/components/Gameboard.js
@@ -1,51 +1,68 @@
-import { Container, Card, Row, Col, Button } from "react-bootstrap";
+import { Container, Card, Row, Col, Button, Image } from "react-bootstrap";
 
 import CP from "./CommandPoint.js"
 import VP from "./VictoryPoint.js"
 import { useState } from "react";
 
-function GamegoardComponent(){
+import left from '../imgs/bg_ig.jpg'
+import right from '../imgs/bg_ba.jpg'
+
+function GamegoardComponent() {
 
     const [roundCounter, setroundCounter] = useState(1);
 
-    return(
-        <Container style={{marginTop: '1%'}}>
-            <Card>
-                <Card.Header>
-                    <Row>
-                        <Col><h1>Kropec</h1></Col>
-                        <Col><h1>VS</h1></Col>
-                        <Col><h1>Blengl boy</h1></Col>
-                    </Row>
-                </Card.Header>
-                <Card.Body>
-                    <Row>
-                        <Col></Col>
-                        <Col><h3>Momentální kolo: {roundCounter}</h3></Col>
-                        <Col></Col>
-                    </Row>
-                </Card.Body>
-                <Card.Footer>
-                    <Button className="btn btn-success" color="success">Další kolo</Button>
-                </Card.Footer>
-            </Card>
-            <Row>
-                <Col>
-                    <CP></CP>
-                </Col>
-                <Col>
-                    <CP></CP>
-                </Col>
-            </Row>
+    return (
+        <div>
+            <div className="bgWrap">
+                <Image src={left} className="bgItem" alt="KEKW">
+                </Image>
+                <Image src={right} className="bgItem" alt="KEKW">
+                </Image>
+            </div>
 
-        
-            <Row>
-                <Col><VP></VP></Col>
-                <Col><VP></VP></Col>
-                
-            </Row>
-        </Container>
-    )}
+            <Container>
 
-export default GamegoardComponent;  
+                <Image src="../../public/imgs/bg_ig.jpg">
+
+                </Image>
+                <Card>
+                    <Card.Header>
+                        <Row>
+                            <Col><h1>Kropec</h1></Col>
+                            <Col><h1>VS</h1></Col>
+                            <Col><h1>Blengl boy</h1></Col>
+                        </Row>
+                    </Card.Header>
+                    <Card.Body>
+                        <Row>
+                            <Col></Col>
+                            <Col><h3>Momentální kolo: {roundCounter}</h3></Col>
+                            <Col></Col>
+                        </Row>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button className="btn btn-success" color="success">Další kolo</Button>
+                    </Card.Footer>
+                </Card>
+                <Row>
+                    <Col>
+                        <CP></CP>
+                    </Col>
+                    <Col>
+                        <CP></CP>
+                    </Col>
+                </Row>
+
+
+                <Row>
+                    <Col><VP></VP></Col>
+                    <Col><VP></VP></Col>
+
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default GamegoardComponent;
 
