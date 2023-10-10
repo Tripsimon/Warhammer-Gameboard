1f6e6169240f943e7f2a484cd9d309444b774ad7
Martin Lešák <47338791+Tripsimon@users.noreply.github.com>
2023-10-10 19:32:26 +0200

    index.go

--- server/index.go
+++ server/index.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
         fmt.Fprintf(w, "Hello!")
     })
 
