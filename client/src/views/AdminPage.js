9bb314e3723909233f7ce2b0a4dad98155444d87
Filipsis <bidlofilip@seznam.cz>
2023-10-28 12:36:11 +0200

    založení herny

--- client/src/views/AdminPage.js
+++ client/src/views/AdminPage.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import {/*doplnit */ } from 'react-bootstrap';
-import { useNavigate  } from "react-router-dom";
-function AdminPage() {
-return(
-<h1>sdsdsd</h1>
-)
-}
+import {Container, Form, Button, Card, Row, Col} from 'react-bootstrap';
 
+function AdminPage() {
+
+    const handleSubmit = (event) => {
+  
+    };
+
+return(
+
+    <div>
+      <Container className='mt-4'>
+        <Card>
+          <Card.Header>
+            <h2> Založení herny </h2>
+          </Card.Header>
+          <Card.Body>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Control type='text' placeholder='Jméno herny'></Form.Control>
+                <Row>
+                  <Col>
+                    <Form.Control type='text' placeholder='Jméno (login)'></Form.Control>
+                  </Col>
+                  <Col>
+                    <Form.Control type='text' placeholder='Heslo'></Form.Control>
+                  </Col>
+                </Row>
+                <Row>
+                 
+                </Row>
+                <Button type='submit'>Založit</Button>
+              </Form.Group>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+
+)
+
+}
 
 export default AdminPage
