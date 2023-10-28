30507107e2d6e8387ba858121a0cb60108b96f12
Martin Lešák <47338791+Tripsimon@users.noreply.github.com>
2023-10-28 17:02:24 +0200

    fdsfds

--- server/dbManager.go
+++ server/dbManager.go
@@ -1,19 +1,3 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	   )
-
-func main()  {
-	fmt.Println("Preparing to connect to the DB ...")
-
-	db, err = sql.Open("mysql", "user:Aa123456@tcp(localhost:3002)/WH")
-
-	fmt.Println(db)
-}
-
-func printStuff(){
-		fmt.Println("hello world")
-}
+func
