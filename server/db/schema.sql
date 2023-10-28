af50443ec6b7c02f9d03608ae8694f94e7da8da3
Martin Lešák <47338791+Tripsimon@users.noreply.github.com>
2023-10-28 13:37:16 +0200

    Upravy schema DB

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,27 @@
-CREATE TABLE Facilities (
-    id int NOT NULL AUTO_INCREMENT KEY,
-    login varchar(255),
-    password varchar(255)
+CREATE TABLE `facilities` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`login` VARCHAR(50) NOT NULL DEFAULT '0',
+	`password` VARCHAR(50) NOT NULL DEFAULT '0',
+	`facilityName` VARCHAR(50) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
 )
+COLLATE='latin1_swedish_ci';
+
+CREATE TABLE `factions` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NULL DEFAULT NULL,
+	`codeName` VARCHAR(50) NULL DEFAULT NULL,
+	`description` VARCHAR(255) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='latin1_swedish_ci';
+
+CREATE TABLE `detachments` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`faction_id` INT NOT NULL DEFAULT 0,
+	`name` VARCHAR(50) NOT NULL DEFAULT '0',
+	`description` VARCHAR(50) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
+)
+COLLATE='latin1_swedish_ci';
+
