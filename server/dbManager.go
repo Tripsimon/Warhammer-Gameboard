b667d3630f3d3e0d2c6c808effbe491be2942cb0
tripsimon <tripsimon3@gmail.com>
2023-11-03 17:54:30 +0100

    kominikace s backendem

--- server/dbManager.go
+++ server/dbManager.go
@@ -8,27 +8,22 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+/*
 func main() {
-	fmt.Println("Připojuji se k DB")
-	db, err := sql.Open("mysql", "user:Aa123456@tcp(localhost:3002)/WH")
 
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
+
+
+
+	DBcreateFacility("Jmeno","Heslo","ScreenJmeno")
+
+
 
 	type Facility struct {
 		Login    string `json:"login"`
 		Password string `json:"password"`
 	}
 
-	insert, err := db.Query("INSERT INTO facilities (login, password, facilityName) VALUES ('Bb','Bb','Bb')")
 
-	if err != nil {
-		panic(err.Error())
-	}
-
-	defer insert.Close()
 
 	var facility Facility
 
@@ -39,5 +34,60 @@ func main() {
 	}
 
 	log.Println(facility.Login, facility.Password)
+
+
+}
+*/
+
+func DBcreateFacility(login string, password string, facilityName string) {
+
+	fmt.Println("Připojuji se k DB")
+	db, err := sql.Open("mysql", "user:Aa123456@tcp(localhost:3002)/WH")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO facilities (login, password, facilityName) VALUES ('" + login + "','" + password + "','" + facilityName + "')")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer insert.Close()
+
+}
+
+func DBGetFacilities() {
+	fmt.Println("Připojuji se k DB")
+	db, err := sql.Open("mysql", "user:Aa123456@tcp(localhost:3002)/WH")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	var (
+		login      string
+		password   string
+		screenName string
+	)
+
+	query, err := db.Query("SELECT * FROM facilities")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for query.Next() {
+		err := query.Scan(&login, &password, &screenName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(login, password, screenName)
+	}
+
+	defer query.Close()
 
 }
--- server/index.go
+++ server/index.go
@@ -2,25 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
-func DBTest() {
+func main() {
+	log.Println("Server se zapíná")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello!")
 	})
 
+	http.HandleFunc("/facility/createFacility", HandleCreateFacility)
+	http.HandleFunc("/facility/getAllFacility", HandleGetAllFacility)
+
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 	http.HandleFunc("/auth", authentification)
 
+	log.Println("Server naslouchá na portu 3001")
 	http.ListenAndServe(":3001", nil)
 
 }
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func HandleGetAllFacility(w http.ResponseWriter, req *http.Request) {
+	enableCors(&w)
+	DBGetFacilities()
+}
+
+func HandleCreateFacility(w http.ResponseWriter, req *http.Request) {
+
+	var login = strings.Join(req.URL.Query()["login"], "")
+	var password = strings.Join(req.URL.Query()["password"], "")
+	var screenName = strings.Join(req.URL.Query()["screenName"], "")
+	fmt.Fprintf(w, "SUCCESS")
+	DBcreateFacility(login, password, screenName)
 }
 
 func authentification(w http.ResponseWriter, req *http.Request) {
