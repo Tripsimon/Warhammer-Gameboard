39058cae9fbea46d3ef481b831a937e732681faf
Martin Lešák <lesak@cis.cz>
2023-11-03 12:06:13 +0100

    Pojmenování imagu

--- server/dbManager.go
+++ server/dbManager.go
@@ -1,27 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func main(){
-	fmt.Println("Připojuji se k DB");
-	db, err := sql.Open("mysql", "user:Aa123456@tcp(localhost:3002)/WH");
+func main() {
+	fmt.Println("Připojuji se k DB")
+	db, err := sql.Open("mysql", "user:Aa123456@tcp(localhost:3002)/WH")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	type Facility struct {
+		Login    string `json:"login"`
+		Password string `json:"password"`
+	}
+
+	insert, err := db.Query("INSERT INTO facilities (login, password, facilityName) VALUES ('Bb','Bb','Bb')")
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	defer db.Close()
+	defer insert.Close()
 
-	insert, err := db.Query("INSERT INTO facilities (login, password, facilityName) VALUES ('Bb','Bb','Bb')")
+	var facility Facility
 
-	if err != nil{
+	err = db.QueryRow("SELECT login, password FROM facilities").Scan(&facility.Login, &facility.Password)
+
+	if err != nil {
 		panic(err.Error())
 	}
 
-	defer insert.Close()
+	log.Println(facility.Login, facility.Password)
 
 }
--- server/index.go
+++ server/index.go
@@ -1,48 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "database/sql"
-    "github.com/go-sql-driver/mysql"
+	"fmt"
+	"net/http"
 )
 
-func main() {
+func DBTest() {
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hello!")
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello!")
+	})
 
-    http.HandleFunc("/hello", hello)
-    http.HandleFunc("/headers", headers)
-    http.HandleFunc("/auth", authentification)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/auth", authentification)
 
-    http.ListenAndServe(":3001", nil)
+	http.ListenAndServe(":3001", nil)
+
 }
 
 func enableCors(w *http.ResponseWriter) {
-    (*w).Header().Set("Access-Control-Allow-Origin", "*")
-    }
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
 
-func authentification(w http.ResponseWriter, req *http.Request){
-    enableCors(&w)
-    fmt.Fprintf(w, "1")
+func authentification(w http.ResponseWriter, req *http.Request) {
+	enableCors(&w)
+	fmt.Fprintf(w, "1")
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
-    fmt.Fprintf(w, "hello\n")
+	fmt.Fprintf(w, "hello\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
 
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
-    }
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
 }
-
