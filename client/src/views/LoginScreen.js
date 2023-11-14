c1bcf83468965bcc1c3fcfbeb42384ac0db00b6e
Martin Lešák <lesak@cis.cz>
2023-11-14 14:40:02 +0100

    Oprava chyb

--- client/src/views/LoginScreen.js
+++ client/src/views/LoginScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
-import { Button, Card, Container, Form, FormCheck } from 'react-bootstrap'
+import { Button, Card, Container, Form } from 'react-bootstrap'
 import axios from "axios"
 
 function LoginScreen() {
@@ -23,25 +23,27 @@ function LoginScreen() {
             })
     };
 
-    const handleAdminLogin = () => {
-        navigate("/adminPage");
-    };
-
     return (
         <div>
             <Container className='mt-4'>
                 <Card>
-                    <Card.Header>
-                        Přihlášení
-                    </Card.Header>
-                    <Card.Body>
-                        <Form onSubmit={handleSubmit}>
+                    <Form onSubmit={handleSubmit}>
+                        <Card.Header>
+                            Přihlášení
+                        </Card.Header>
+                        <Card.Body>
+
                             <Form.Group>
-                                <Form.Control type='text' placeholder='Zadejte token'></Form.Control>
-                                <Button type='submit'>Připojit</Button>
+                                <Form.Control type='text' placeholder='Login' ></Form.Control>
+                                <Form.Control type='text' placeholder='Heslo' className='mt-2'></Form.Control>
+
                             </Form.Group>
-                        </Form>
-                    </Card.Body>
+
+                        </Card.Body>
+                        <Card.Footer>
+                            <Button type='submit'>Připojit</Button>
+                        </Card.Footer>
+                    </Form>
                 </Card>
             </Container>
         </div>
