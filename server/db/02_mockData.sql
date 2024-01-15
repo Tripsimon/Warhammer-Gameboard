0cd5f772a4cd48d5f704ae639467b01e8d9294f5
Filipsis <bidlofilip@seznam.cz>
2024-01-15 19:05:01 +0100

    Mock data

--- server/db/02_mockData.sql
+++ server/db/02_mockData.sql
@@ -9,9 +9,42 @@ INSERT INTO `WH`.`factions` (`name`, `codeName`, `description`) VALUES ('Chaos D
 INSERT INTO `WH`.`factions` (`name`, `codeName`, `description`) VALUES ('Ultramarines', 'spaceMarines', 'Placeholder Popis');
 
 
-INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('1', 'Index Detachment', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('1', 'Detachment n. 1', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('1', 'Detachment n. 2', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('2', 'Detachment n. 1', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('2', 'Detachment n. 2', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('3', 'Detachment n. 3', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('3', 'Detachment n. 4', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('4', 'Detachment n. 3', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('4', 'Detachment n. 4', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('5', 'Detachment n. 5', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('5', 'Detachment n. 6', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('6', 'Detachment n. 5', 'placeholder');
+INSERT INTO `WH`.`detachments` (`faction_id`, `name`, `description`) VALUES ('6', 'Detachment n. 6', 'placeholder');
 
-INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('-1', 'Tactical Re-Roll', 'Dovolí uživately znovu hodit jednu kostku', '1');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('1', 'Tactical Re-Roll', 'Dovoli uzivateli znovu hodit jednu kostku', '1');
 INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('1', 'Miracle', 'Gain one more Miracle dice', '2');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('2', 'Another one', 'Gain one more Miracle dice', '3');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('2', 'Tactical Re-Roll', 'Dovoli uzivateli znovu hodit jednu kostku', '1');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('3', 'Miracle', 'Gain one more Miracle dice', '2');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('3', 'Another one', 'Gain one more Miracle dice', '3');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('4', 'Tactical Re-Roll', 'Dovoli uzivateli znovu hodit jednu kostku', '1');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('4', 'Miracle', 'Gain one more Miracle dice', '2');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('5', 'Another one', 'Gain one more Miracle dice', '3');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('5', 'Tactical Re-Roll', 'Dovoli uzivateli znovu hodit jednu kostku', '1');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('6', 'Miracle', 'Gain one more Miracle dice', '2');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('6', 'Another one', 'Gain one more Miracle dice', '3');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('7', 'Tactical Re-Roll', 'Dovoli uzivateli znovu hodit jednu kostku', '1');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('7', 'Miracle', 'Gain one more Miracle dice', '2');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('8', 'Another one', 'Gain one more Miracle dice', '3');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('8', 'Tactical Re-Roll', 'Dovoli uzivateli znovu hodit jednu kostku', '1');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('9', 'Miracle', 'Gain one more Miracle dice', '2');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('9', 'Another one', 'Gain one more Miracle dice', '3');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('10', 'Tactical Re-Roll', 'Dovoli uzivateli znovu hodit jednu kostku', '1');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('10', 'Miracle', 'Gain one more Miracle dice', '2');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('11', 'Another one', 'Gain one more Miracle dice', '3');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('11', 'Tactical Re-Roll', 'Dovoli uzivateli znovu hodit jednu kostku', '1');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('12', 'Miracle', 'Gain one more Miracle dice', '2');
+INSERT INTO `WH`.`stratagems` (`detachment_id`, `name`, `ability`, `price`) VALUES ('12', 'Another one', 'Gain one more Miracle dice', '3');
 
 INSERT INTO `WH`.`settings` (`key1`, `value1`) VALUES ('admin', '$2a$10$98mmJBA4OX2MYWzVMY88cupwTLJuchW9vi6L8n25iMQr3H0oHjGbG'); 
